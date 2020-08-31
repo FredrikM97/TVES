a9283d86b4982842343f5aeef92708cfe4c23fa9
FredrikM97 <fredrik9779@gmail.com>
2020-08-31 15:37:55 +0200

    Setup

---
+++ Project1/main.java
@@ -0,0 +1,66 @@
+abstract class Car {
+    abstract void MoveForward();
+    abstract void MoveBackward();
+    abstract boolean isEmpty();
+    abstract void Park();
+    abstract void UnPark();
+    abstract void WhereIs();
+}
+
+class Tesla extends Car {
+    int x = 5;
+  
+    void MoveForward(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+    boolean isEmpty(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+    void MoveBackward(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+    void Park(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+    void UnPark(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+    void WhereIs(){
+        /**
+        Description
+        Pre-condition:
+        Post-condition:
+        Test-cases:
+        */
+    }
+}
+
+public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+}
