6299c41ab952cbf1ffff40fdcf886b952018d23b
FredrikM97 <fredrik9779@gmail.com>
2020-09-07 00:22:33 +0200

    files

---
+++ Lectures.md
@@ -0,0 +1,23 @@
+# Lecture 1 (2020-08-31)
+
+Failt, error, failure
+Functional and structural testing (unit, integration, system)
+Finding bugs:
+    * Testing
+    * Fault classification
+    * Fault isolation
+
+Static analysis:
+    * Test abstract properties without running (unintialized/unused variables, empty/unspecified cases, antipattern)
+
+Model checking:
+    * Test the state-space for formally specified properties
+    855556987+
+
+Whitebox testing and blackbox testing
+Specification: System testing
+Detailed design: Functional testing
+Implementation code: unit test
+
+
+3233,3,2,32,63333333
---
+++ Project1/unitTest.java
@@ -0,0 +1,77 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class unitTest {
+
+	@Test
+	
+	/**
+    Move car 1 meter forward, returns current position and situation of detected parking spaces
+    @throws IndexOutOfBoundsException	if position < 500 ({@code position < 500})
+    Pre-condition:
+    	* Car position < 500
+    	* 
+    Post-condition:
+    Test-cases:
+    */
+	void MoveForward(){
+        
+    }
+	
+	/**
+    Query sensors and filter noise based on 5 input signals, Return the distance of nearest object
+    @param 	sensors 					Array of sensors
+    @throws	IndexOutOfBoundsException	if length of sensor array is smaller than 5 ({@code len(sensors) < 5})
+    											
+    @return			 
+    Pre-condition:
+     	* Query len(sensors) >= 5
+    Post-condition:
+    Test-cases:
+    */
+	void isEmpty(){
+        
+    }
+	
+	/**
+    Move car 1 meter backwards, returns current position and situation of detected parking spaces
+    Pre-condition:
+    * Car position > 0
+    Post-condition:
+    Test-cases:
+    */
+    void MoveBackward(){
+        
+    }
+    
+    /**
+    Description
+    Pre-condition:
+    Post-condition:
+    Test-cases:
+    */
+    void Park(){
+        
+    }
+    
+    /**
+    Description
+    Pre-condition:
+    Post-condition:
+    Test-cases:
+    */
+    void UnPark(){
+        
+    }
+    
+    /**
+    Description
+    Pre-condition:
+    Post-condition:
+    Test-cases:
+    */
+    void WhereIs(){
+        
+    }
+}
